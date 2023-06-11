15eed34183a2384534aa9cfe668225665ce8e115
becshark-vHqLqV <becshark@hotmail.com>
2023-06-11 14:04:43 +0800

    updating files

---
+++ QuickDBD-project 2.sql
@@ -0,0 +1,71 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/48nmtB
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [Category] (
+    [category_id] INT  NOT NULL ,
+    [category] VARCHAR  NOT NULL ,
+    CONSTRAINT [PK_Category] PRIMARY KEY CLUSTERED (
+        [category_id] ASC
+    )
+)
+
+CREATE TABLE [subcategory] (
+    [subcategory_id] VARCHAR  NOT NULL ,
+    [subcategory] VARCHAR  NOT NULL ,
+    CONSTRAINT [PK_subcategory] PRIMARY KEY CLUSTERED (
+        [subcategory_id] ASC
+    )
+)
+
+CREATE TABLE [contacts] (
+    [contact_id] INT  NOT NULL ,
+    [first_name] VARCHAR  NOT NULL ,
+    [last_name] VARCHAR  NOT NULL ,
+    [email] VARCHAR  NOT NULL ,
+    CONSTRAINT [PK_contacts] PRIMARY KEY CLUSTERED (
+        [contact_id] ASC
+    )
+)
+
+CREATE TABLE [campaign] (
+    [cf_id] INT  NOT NULL ,
+    [contact_id] INT  NOT NULL ,
+    [company_name] VARCHAR  NOT NULL ,
+    [project_description] VARCHAR  NOT NULL ,
+    [goal] INT  NOT NULL ,
+    [pledged] INT  NOT NULL ,
+    [outcome] VARCHAR  NOT NULL ,
+    [backers_count] INT  NOT NULL ,
+    [country] VARCHAR  NOT NULL ,
+    [currency] VARCHAR  NOT NULL ,
+    [launch_date] DATE  NOT NULL ,
+    [end_date] DATE  NOT NULL ,
+    [category_id] INT  NOT NULL ,
+    [subcategory_id] INT  NOT NULL ,
+    CONSTRAINT [PK_campaign] PRIMARY KEY CLUSTERED (
+        [cf_id] ASC
+    )
+)
+
+ALTER TABLE [campaign] WITH CHECK ADD CONSTRAINT [FK_campaign_contact_id] FOREIGN KEY([contact_id])
+REFERENCES [contacts] ([contact_id])
+
+ALTER TABLE [campaign] CHECK CONSTRAINT [FK_campaign_contact_id]
+
+ALTER TABLE [campaign] WITH CHECK ADD CONSTRAINT [FK_campaign_category_id] FOREIGN KEY([category_id])
+REFERENCES [Category] ([category_id])
+
+ALTER TABLE [campaign] CHECK CONSTRAINT [FK_campaign_category_id]
+
+ALTER TABLE [campaign] WITH CHECK ADD CONSTRAINT [FK_campaign_subcategory_id] FOREIGN KEY([subcategory_id])
+REFERENCES [subcategory] ([subcategory_id])
+
+ALTER TABLE [campaign] CHECK CONSTRAINT [FK_campaign_subcategory_id]
+
+COMMIT TRANSACTION QUICKDBD
--- Resources/ETL Crowdfunding schema final.sql
+++ Resources/ETL Crowdfunding schema final.sql
@@ -1,17 +1,3 @@
----Create Table and Import Data From .csv Files-------------------------
----------------------------------Creat Data Import Directory---------------------------------
-CREATE PROCEDURE data_import(tablename varchar,variable varchar)
-LANGUAGE plpgsql
-AS $$
-DECLARE
----------------------------------------------------------------------------------------------
---      Before run, modify the csv path to the location of the csv files on your system.   --
----------------------------------------------------------------------------------------------
-tabdir varchar = 'C:\git bash\Crowdfunding_ETL\Resources'; 
-dir varchar = CONCAT(tabdir,variable,'.csv');
-BEGIN
-EXECUTE format('COPY %I FROM %L DELIMITER '','' CSV HEADER',tablename,dir);
-END; $$
 ---------------------------------------- Contacts Table -------------------------------------
 ---------------------------------------------------------------------------------------------
 -- Create a Contacts Table
@@ -20,28 +6,19 @@ CREATE TABLE contacts (
     first_name varchar(255) NOT NULL,
 	last_name varchar(255) NOT NULL,
 	email varchar(255) NOT NULL);
---Import Contacts Data from .csv File
-CALL data_import('contacts','contacts')
---Contacts Table View
-SELECT*FROM contacts
+SELECT * FROM contacts
 ---------------------------------------- Category Table -------------------------------------
 ---------------------------------------------------------------------------------------------
 -- Create a Category Table
 CREATE TABLE category (
     category_id char(4) PRIMARY KEY NOT NULL,
     category varchar(255) NOT NULL);
---Import Category Data from .csv File
-CALL data_import('category','category')
---Category Table View
-SELECT*FROM category
+SELECT * FROM category
 ---------------------------------------- Subcategory Table ----------------------------------
 -- Create a Subcategory Table
 CREATE TABLE subcategory (
     subcategory_id varchar(10) PRIMARY KEY NOT NULL,
     subcategory varchar(255) NOT NULL);
---Import Subcategory Data from .csv File
-CALL data_import('subcategory','subcategory')
---Subcategory Table View
 SELECT*FROM subcategory
 -------------------------------------- Campaign Table ---------------------------------------
 -- Create a Campaign Table
@@ -63,8 +40,5 @@ CREATE TABLE campaign (
 		FOREIGN KEY (category_id) REFERENCES category(category_id),
 	subcategory_id varchar(10) NOT NULL,
 		FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id));
---Import Campaign Data from .csv File
-CALL data_import('campaign','campaign')
---Campaign Table View
 SELECT*FROM campaign
 ---------------------------------------------------------------------------------------------
